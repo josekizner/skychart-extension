fef1eb189a8bbf6257cf410da12985963fe73532
josekizner <jose.kizner@mondshipping.com.br>
2026-03-11 19:15:35 -0300

    Nova API key Gemini

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = "AIzaSyByqiMDFdNrGOKLb-1BmId2ne_PcVCE1Ew";
+const GEMINI_API_KEY = "AIzaSyAUJQghRHkjEnM4HQCeVF_6LuS2iTQy-KQ";
 const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 // ===== AUTO-UPDATE =====
